20e7ecc23699a8e467be4b6f98d8f1fc75b28318
bala-amypo <bala@amypo.in>
2025-12-18 09:32:51 +0000

    User commit

--- src/main/exception/ResourceNotFoundException.java
+++ src/main/exception/ResourceNotFoundException.java
@@ -3,10 +3,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler{
-
     @ExceptionHandler(ResourceNotFoundException.class)
         public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){
             return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
