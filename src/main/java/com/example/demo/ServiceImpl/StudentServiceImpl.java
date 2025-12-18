3e42b64064b39f8aa27c8c602bb721f59c4f10db
bala-amypo <bala@amypo.in>
2025-12-18 10:18:38 +0000

    User commit

--- src/main/java/com/example/demo/ServiceImpl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/ServiceImpl/StudentServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
-com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
 public class StudentServiceImpl implements StudentService {
